48a5a5ff0b574c93f1d57d76ee99c53d58cd599c
Bruno Ricci <br9191fr@gmail.com>
2026-02-12 14:58:16 +0100

    A try with resources

--- src/main/java/com/cecurity/Main.java
+++ src/main/java/com/cecurity/Main.java
@@ -31,6 +31,7 @@ public class Main {
             test_gcm_256(false);
             test_gcm_256(false);
             test_tls1();
+            writeToFileZipFileContents("test.zip", "output.txt");
         } catch (Exception e) {
             //e.printStackTrace();
             System.out.println("In main: Exception: " + e.getMessage());
@@ -176,5 +177,41 @@ public class Main {
         System.out.println("store saved");
         fOut.close();
         IO.println("test_tls1 stop");
+    }
+    public static void writeToFileZipFileContents(String zipFileName,
+                                                  String outputFileName)
+            throws java.io.IOException {
+
+        java.nio.charset.Charset charset =
+                java.nio.charset.StandardCharsets.US_ASCII;
+        java.nio.file.Path outputFilePath =
+                java.nio.file.Paths.get(outputFileName);
+
+        // Open zip file and create output file with
+        // try-with-resources statement
+        IO.println("In writeToFileZipFileContents: zipFileName = " + zipFileName);
+        try (
+                java.util.zip.ZipFile zf =
+                        new java.util.zip.ZipFile(zipFileName);
+                java.io.BufferedWriter writer =
+                        java.nio.file.Files.newBufferedWriter(outputFilePath, charset)
+        ) {
+            // Enumerate each entry
+            IO.println("Enter loop");
+            int count = 0;
+            for (java.util.Enumeration entries =
+                 zf.entries(); entries.hasMoreElements();) {
+                // Get the entry name and write it to the output file
+                String newLine = System.getProperty("line.separator");
+                String zipEntryName =
+                        ((java.util.zip.ZipEntry)entries.nextElement()).getName() +
+                                newLine;
+                //IO.println("zipEntryName = " + zipEntryName);
+                writer.write(zipEntryName, 0, zipEntryName.length());
+                count++;
+            }
+            IO.println("Found "+count+" entries");
+        }
+        IO.println("End of writeToFileZipFileContents: zipFileName");
     }
 }
